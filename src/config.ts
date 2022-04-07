a194242a8c25ed517758586abedb157cd53efc44
mobile-michel <info@maillard.li>
2022-04-07 16:50:01 +0200

    Intermediate update

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+export const SITE = {
+    title: 'Au Coeur des Sons',
+    description: 'Au Coeur des Sons, des soins avec la sonothérapie',
+    footer: '© 2022 Au Coeur des Sons Copyright | info@sonotherapie.ch'
+}
+
+export const NAV_ITEMS = {
+    accueil: {
+        path: '/',
+        title: 'ACCUEIL'
+    },
+    soins: {
+        path: '/soins',
+        title: 'SOINS'
+    },
+    formation: {
+        path: '/formation',
+        title: 'FORMATION'
+    },
+    blog: {
+        path: '/blog',
+        title: 'BLOG'
+    },
+    shop: {
+        path: '/shop',
+        title: 'SHOP'
+    }
+}
