1b9c451df981545a0effc7ef10b430352b379e76
mobile-michel <info@maillard.li>
2022-04-08 16:43:44 +0200

    Intermediate update

--- src/pages/contenu/contenu copy.ts
+++ src/pages/contenu/contenu copy.ts
@@ -1,3 +1,4 @@
+// Contenu pour la page SOINS
 export const SOINS = {
     voyage: {
         titre: "Voyage sonore",
@@ -14,3 +15,9 @@ export const SOINS = {
         photo: "/assets/1920x1080-table-harmonie.jpg"
     },
 }
+
+// Contenu pour la page FORMATION
+
+// Contenu pour la page BLOG
+
+// Contenu pour la page SHOP
