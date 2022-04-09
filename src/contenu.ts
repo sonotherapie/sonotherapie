747ccbae9be1a159bb5f1a3ae69ad5f0759f9394
mobile-michel <67042898+mobile-michel@users.noreply.github.com>
2022-04-09 16:10:06 +0200

    Update contenu.ts

--- src/contenu.ts
+++ src/contenu.ts
@@ -13,25 +13,25 @@ export const SOINS = {
         lien: "#table",
         photo: "/assets/1920x1080-table-harmonie.jpg"
     },
-    jalaka: {
-        titre: "Vapakaia Energy",
-        description: "Combinaison du soin table d'harmonie et bols chantants tibétains",
-        prix: "1h à 1h30 / CHF 120.-",
-        lien: "#jalaka",
-        photo: "/assets/2022-massage-sonore.jpg"
-    },
-    vapakaia: {
-        titre: "Vapakaìa",
-        description: "Le Vapa-Kaìa, basé sur une technique ancestrale, est un mélange de doux massages par pressions sur le corps, accompagnés par les sons magiques et les vibrations de bols chantants thérapeutiques, l’utilisation d’huiles essentielles et de pierres chaudes",
-        prix: "1h à 1h30 / CHF 120.-",
-        lien: "#vapakaia",
-        photo: "/assets/1920x1080-chambre-vue-generale.jpg"
-    },
     ayuma: {
         titre: "Ayuma",
         description: "Ayuma est un soin énergique qui apporte, par son toucher apaisant et le son des bols, du calme et de la sérénité. Il nous réaligne, ouvre notre cœur et prodigue de la douceur pour soi-même.",
         prix: "1h à 1h30 / CHF 120.-",
         lien: "#ayuma",
         photo: "/assets/2022-logo-ayuma.png"
+    },
+    vapakaia: {
+        titre: "Vapa Kaia",
+        description: "Le Vapa-Kaìa, basé sur une technique ancestrale, est un mélange de doux massages par pressions sur le corps, accompagnés par les sons magiques et les vibrations de bols chantants thérapeutiques, l’utilisation d’huiles essentielles et de pierres chaudes",
+        prix: "1h à 1h30 / CHF 120.-",
+        lien: "#vapakaia",
+        photo: "/assets/1920x1080-chambre-vue-generale.jpg"
+    },
+    jalaka: {
+        titre: "Vapa Kaia Energy",
+        description: "Combinaison du soin table d'harmonie et bols chantants tibétains",
+        prix: "1h à 1h30 / CHF 120.-",
+        lien: "#jalaka",
+        photo: "/assets/2022-massage-sonore.jpg"
     }
 }
